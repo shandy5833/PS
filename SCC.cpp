43aa18dd5d86f704cf6c58ae767a18784f66d69c
shandy5833 <83273783+shandy5833@users.noreply.github.com>
2024-09-11 00:51:45 +0900

    Create SCC.cpp

---
+++ SCC.cpp
@@ -0,0 +1,43 @@
+const ll MAXN = 101000;
+ll fin[MAXN], vis[MAXN], sn[MAXN];
+vector<vector<ll>> SCC;
+ll n, m, u, v, id, SN;
+vector<ll> adj[MAXN];
+stack<ll> st;
+
+ll D(ll x) {
+    vis[x] = ++id;
+    st.push(x);
+    ll par = vis[x];
+    for (auto i : adj[x]) {
+        if (!vis[i]) par = min(par, D(i));
+        else if (!fin[i]) par = min(par, vis[i]);
+    }
+    if (par == vis[x]) {
+        vector<ll> scc;
+        while (1) {
+            ll cur = st.top();
+            st.pop();
+            scc.push_back(cur);
+            fin[cur] = true;
+            sn[cur] = SN + 1;
+            if (cur == x) break;
+        }
+        sort(scc.begin(), scc.end());
+        SCC.push_back(scc);
+        SN++;
+    }
+    return par;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    cin >> n >> m;
+    for (ll i = 0; i < m; i++)
+        cin >> u >> v, adj[u].push_back(v);
+    for (ll i = 1; i <= n; i++)
+        if (!vis[i]) D(i);
+
+    return 0;
+}
