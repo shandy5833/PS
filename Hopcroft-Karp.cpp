198e02bb88a452d8da3768daa4b4ac87a8cc4f6c
shandy5833 <83273783+shandy5833@users.noreply.github.com>
2024-09-13 14:46:45 +0900

    Update Hopcroft-Karp.cpp

--- Hopcroft-Karp.cpp
+++ Hopcroft-Karp.cpp
@@ -2,7 +2,7 @@ const ll MAXN = 1010, MAXM = 1010;
 ll dis[MAXN], l[MAXN], r[MAXM], vis[MAXN];
 vector<ll> adj[MAXN];
 
-bool B(ll x) {
+bool BFS(ll x) {
     queue<ll> q;
     bool ret = 0;
     memset(dis, 0, sizeof(dis));
@@ -19,11 +19,11 @@ bool B(ll x) {
     return ret;
 }
 
-bool D(ll x) {
+bool DFS(ll x) {
     if (vis[x]) return 0;
     vis[x] = 1;
     for (auto &i : adj[x]) {
-        if (r[i] == -1 || (!vis[r[i]] && dis[r[i]] == dis[x] + 1 && D(r[i]))) {
+        if (r[i] == -1 || (!vis[r[i]] && dis[r[i]] == dis[x] + 1 && DFS(r[i]))) {
             l[x] = i, r[i] = x;
             return 1;
         }
@@ -31,14 +31,14 @@ bool D(ll x) {
     return 0;
 }
 
-ll M(ll x) {
+ll Match(ll x) {
     memset(l, -1, sizeof(l));
     memset(r, -1, sizeof(r));
     ll ret = 0;
-    while (B(x)) {
+    while (BFS(x)) {
         memset(vis, 0, sizeof(vis));
         for (ll i = 1; i <= x; i++)
-            if (l[i] == -1 && D(i)) ret++;
+            if (l[i] == -1 && DFS(i)) ret++;
     }
     return ret;
 }
