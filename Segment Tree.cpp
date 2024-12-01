f4cb03f2eb1d7295b514b036924e603141c67172
shandy5833 <83273783+shandy5833@users.noreply.github.com>
2024-12-01 19:18:00 +0900

    Update Segment Tree.cpp

--- Segment Tree.cpp
+++ Segment Tree.cpp
@@ -1,12 +1,12 @@
-vector<int> tree;
-int n;
+vector<ll> tree(n << 2, 0);
+ll n;
 
-void U(int p, int val) {
+void U(ll p, ll val) {
     for (tree[p += n] = val; p > 1; p >>= 1)
         tree[p >> 1] = tree[p] + tree[p ^ 1];
 }
-int Q(int l, int r) {
-    int ret = 0;
+ll Q(ll l, ll r) {
+    ll ret = 0;
     for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
         if (l & 1) ret += tree[l++];
         if (r & 1) ret += tree[--r];
