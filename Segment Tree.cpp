5d21d513c8d6b69820d126f0abefb104df7fe6c3
shandy5833 <83273783+shandy5833@users.noreply.github.com>
2024-12-01 19:27:33 +0900

    Update Segment Tree.cpp

--- Segment Tree.cpp
+++ Segment Tree.cpp
@@ -1,15 +1,24 @@
-vector<ll> tree(n << 2, 0);
+vector<ll> tree;
 ll n;
 
-void U(ll p, ll val) {
+void Upd(ll p, ll val) { // update pth idx into val
     for (tree[p += n] = val; p > 1; p >>= 1)
         tree[p >> 1] = tree[p] + tree[p ^ 1];
 }
-ll Q(ll l, ll r) {
+ll Qry(ll l, ll r) { // query on range [l, r)
     ll ret = 0;
     for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
         if (l & 1) ret += tree[l++];
         if (r & 1) ret += tree[--r];
     }
     return ret;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    cin >> n;
+    tree.resize(n << 2, 0);
+
+    return 0;
 }
