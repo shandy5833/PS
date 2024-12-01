1058ca34a1c76588b99751bb3abbf5295584901c
shandy5833 <83273783+shandy5833@users.noreply.github.com>
2024-12-01 18:49:00 +0900

    Iterative Segment Tree

--- Segment Tree.cpp
+++ Segment Tree.cpp
@@ -1,33 +1,15 @@
-const ll maxV = 1000000;
+vector<int> tree;
+int n;
 
-struct Seg {
-    ll arr[maxV], tree[4 * maxV];
-    void Init(ll N, ll s, ll e) {
-        if (s == e) {
-            tree[N] = arr[s];
-            return;
-        }
-        ll mid = (s + e) / 2;
-        Init(N * 2, s, mid);
-        Init(N * 2 + 1, mid + 1, e);
-        tree[N] = tree[N * 2] + tree[N * 2 + 1];
+void U(int p, int val) {
+    for (tree[p += n] = val; p > 1; p >>= 1)
+        tree[p >> 1] = tree[p] + tree[p ^ 1];
+}
+int Q(int l, int r) {
+    int ret = 0;
+    for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+        if (l & 1) ret += tree[l++];
+        if (r & 1) ret += tree[--r];
     }
-    void Update(ll N, ll s, ll e, ll idx, ll val) {
-        if (s == e) {
-            if (s == idx) tree[N] = val;
-            return;
-        }
-        ll mid = (s + e) / 2;
-        if (idx <= mid)
-            Update(N * 2, s, mid, idx, val);
-        else
-            Update(N * 2 + 1, mid + 1, e, idx, val);
-        tree[N] = tree[N * 2] + tree[N * 2 + 1];
-    }
-    ll Query(ll N, ll s, ll e, ll l, ll r) {
-        if (r < s || e < l) return 0;
-        if (l <= s && e <= r) return tree[N];
-        ll mid = (s + e) / 2;
-        return Query(N * 2, s, mid, l, r) + Query(N * 2 + 1, mid + 1, e, l, r);
-    }
-};
+    return ret;
+}
