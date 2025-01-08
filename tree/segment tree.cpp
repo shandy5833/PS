4dbe72175a88d4bda5e584ee82c59dc2a0c67a88
shandy5833 <83273783+shandy5833@users.noreply.github.com>
2025-01-08 19:34:04 +0900

    .

---
+++ tree/segment tree.cpp
@@ -0,0 +1,27 @@
+template <typename T> struct seg {
+    ll n;
+    T id;
+    vector<T> t;
+    T (*merge)(T, T);
+    seg(ll N, T ID, T (*_merge)(T, T)) : n(N), id(ID), merge(_merge) {
+        t.resize(N << 1, id);
+    }
+    void update(ll p, T val) {
+        for (t[p += n] = val; p > 1; p >>= 1) {
+            if (p & 1) t[p >> 1] = merge(t[p ^ 1], t[p]);
+            else t[p >> 1] = merge(t[p], t[p ^ 1]);
+        }
+    }
+    T query(ll l, ll r) {
+        T lret = id, rret = id;
+        for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+            if (l & 1) lret = merge(lret, t[l++]);
+            if (r & 1) rret = merge(t[--r], rret);
+        }
+        return merge(lret, rret);
+    }
+};
+
+/*
+
+*/
